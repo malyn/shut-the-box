161dc7f8b96de877ae7b475fef24f906ee1ef497
Michael Alyn Miller <malyn@strangeGizmo.com>
2020-04-25 15:21:13 -0700

    Do not require `config.edn`
    Prod deployments use environment variables instead of `config.edn`, and
    macchiato.env crashes if it cannot find the config file.

--- src/main/shut_the_box/server/config.cljs
+++ src/main/shut_the_box/server/config.cljs
@@ -1,9 +1,17 @@
 (ns shut-the-box.server.config
   (:require
     [macchiato.env :as config]
+    [macchiato.fs :as fs]
     [mount.core :refer [defstate]]))
 
 (defstate env
-  :start (-> (config/env)
-             (update-in [:host] (fnil identity "localhost"))
+  ;; Note that `config.edn` is optional since, in some environments
+  ;; (Production) we only use environment variables. macchiato.env/env
+  ;; crashes if it cannot find the config file, so we check for that
+  ;; first and then just load from the environment if the file does not
+  ;; exist.
+  :start (-> (if (fs/exists? "config.edn")
+               (config/env)
+               (config/env-props))
+             (update-in [:host] (fnil identity "0.0.0.0"))
              (update-in [:port] (fnil identity 3000))))
